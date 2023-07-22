d5893acee5c5b1b229c0782e4b4914e5d8cbcf60
Pablo Agüera Hernández <paahhe@usal.es>
2023-07-22 11:11:59 +0200

    .

--- __main__.py
+++ __main__.py
@@ -7,7 +7,7 @@ def main():
     searchers = Scrappers()
     
     game = input('Enter the game you want to search: ')
-    
+    game = game.replace("'","")
     steam_price = searchers.steamScrapper(game)
     eneba_price = searchers.enebaScrapper(game)
     gog_price = searchers.gog_Scrapper(game)
--- utils/helpers.py
+++ utils/helpers.py
@@ -27,16 +27,22 @@ def giveFormat(game):
 
 
 def find_first_url_eneba(game):
-    url_to_trie = {
-    
-        "key_europe": "https://www.eneba.com/es/steam-"+game+"-steam-key-europe",
+    url_to_try = {
+        "rockstar_key": "https://www.eneba.com/es/rockstar-social-club-"+game+"-premium-online-edition-rockstar-games-launcher-key-europe",
+        "remakes": "https://www.eneba.com/es/steam-"+game+"-remake-steam-key-global",
+        "pc_global": "https://www.eneba.com/es/steam-"+game+"-pc-steam-key-global",
+        "pc_europe": "https://www.eneba.com/es/steam-"+game+"-pc-steam-key-europe", 
+        "key_europe": "https://www.eneba.com/es/steam-"+game+"-steam-key-global",
         "gog_key": "https://www.eneba.com/es/gog-"+game+"-gog-com-key",
-        
+        "others": "https://www.eneba.com/es/other-"+game+"-pc-official-website-key-europe"
     }
     
-    for value in url_to_trie.values():
+    for value in url_to_try.values():
         
         sol = check_availability(value)
         
         if sol != False:
             return sol
+        
+        
+    
